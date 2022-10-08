dc3a3d68b076d96c51a279fc56d8da42bca4410a
Laura Kambanis <laurakambanis@Lauras-MacBook-Air-3.local>
2022-10-08 13:37:08 +0100

    models finished

---
+++ models/index.js
@@ -0,0 +1,41 @@
+//required all modules
+
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+//user has many posts
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+//posts belong to user
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+//post has many comments
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+//comment belongs to post
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+//user has many comments
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+//comment belongs to user
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Comment, Post };
