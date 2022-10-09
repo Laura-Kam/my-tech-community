5cf53d5b8403d42ebccc64f3bd05bebf36959eb7
Laura Kambanis <laurakambanis@Lauras-Air-3.home>
2022-10-09 21:52:43 +0100

    attempt to create log in

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
-const routes = require("./controllers");
+const routes = require("./controllers/");
 const session = require("express-session");
 require("dotenv").config();
 
