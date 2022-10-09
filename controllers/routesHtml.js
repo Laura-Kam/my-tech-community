1cad67a28343c79dbfef5452a47d0fd0124a73eb
Laura Kambanis <laurakambanis@Lauras-MacBook-Air-3.local>
2022-10-09 11:47:03 +0100

    attempted to render a homepage

--- controllers/routesHtml.js
+++ controllers/routesHtml.js
@@ -1,7 +1,32 @@
 const router = require("express").Router();
 
-const { User } = require("../models");
+const { Post, User } = require("../models");
 
+// GET all posts for homepage
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      attributes: ["id", "title", "date-created", "user_id", "post_paragraph"],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    const postsSerialized = posts.map((post) => post.get({ plain: true }));
+    const obj = { posts: postsSerialized, logged_in: req.session.logged_in };
+
+    res.render("homepage", obj);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Send over the 'loggedIn' session variable to the 'homepage' template
+
+//log in route
 router.get("/login", (req, res) => {
   res.render("login");
 });
